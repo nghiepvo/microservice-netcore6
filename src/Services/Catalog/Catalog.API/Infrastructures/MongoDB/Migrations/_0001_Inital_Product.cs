956a22b215c8b77ed048fb08edbee2882b835ca9
Nghiep Vo <nghiep.voke@gmail.com>
2022-05-02 18:14:19 +0700

    [Catalog] upgrade coverage code

--- src/Services/Catalog/Catalog.API/Infrastructures/MongoDB/Migrations/_0001_Inital_Product.cs
+++ src/Services/Catalog/Catalog.API/Infrastructures/MongoDB/Migrations/_0001_Inital_Product.cs
@@ -1,8 +1,9 @@
 using MongoDB.Entities;
 using Catalog.API.Infrastructures.MongoDB.MasterData;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Catalog.API.Infrastructures.MongoDB.Migrations;
-
+[ExcludeFromCodeCoverage]
 public class _0001_Inital_Product : IMigration
 {
     public async Task UpgradeAsync()
